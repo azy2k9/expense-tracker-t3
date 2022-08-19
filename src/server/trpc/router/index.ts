6e29db4eb8963e094ad25d1c6764366f8818fbfc
evergreen-arslaan-qadus <74248128+evergreen-arslaan-qadus@users.noreply.github.com>
2022-08-19 15:18:23 +0100

    prettier the whole project

--- src/server/trpc/router/index.ts
+++ src/server/trpc/router/index.ts
@@ -1,11 +1,11 @@
 // src/server/trpc/router/index.ts
-import { t } from "../utils";
-import { exampleRouter } from "./example";
-import { authRouter } from "./auth";
+import { t } from '../utils';
+import { exampleRouter } from './example';
+import { authRouter } from './auth';
 
 export const appRouter = t.router({
-  example: exampleRouter,
-  auth: authRouter,
+    example: exampleRouter,
+    auth: authRouter,
 });
 
 // export type definition of API
