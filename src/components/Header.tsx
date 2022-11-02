46400714ffe6ab6f39d98cebf4c89a946d44cfd2
Arslaan Qadus <azy2k9@gmail.com>
2022-11-02 23:05:38 +0000

    Header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,37 +9,34 @@ const Header = () => {
 
     return (
         <div className="text-white bg-indigo-500 flex items-center">
-            <h1
-                className="text-4xl md:text-[4rem] font-extrabold flex-1 text-center py-4
-                    h-[10vh]"
-            >
+            <h1 className="text-4xl md:text-[4rem] font-extrabold flex-1 text-center pt-10 md:pt-16 min-h-[10vh] md:max-h-[10vh]">
                 <Link href={'/'}>
                     <span className="hover:cursor-pointer">
                         Expense <span className="text-purple-300">Tracker</span>
                     </span>
                 </Link>
             </h1>
-            <div className="flex flex-col pr-2">
-                {session && session.user?.image && (
-                    <div className="flex justify-center pb-1">
-                        <Image
-                            alt={session.user.name || 'profile picture'}
-                            className="rounded-full"
-                            src={session.user?.image}
-                            height={50}
-                            width={50}
-                        />
-                    </div>
-                )}
-                {session && session.user && (
+            {session && session.user && (
+                <div className="flex flex-col pr-2">
+                    {session && session.user?.image && (
+                        <div className="flex justify-center pb-1">
+                            <Image
+                                alt={session.user.name || 'profile picture'}
+                                className="rounded-full"
+                                src={session.user?.image}
+                                height={50}
+                                width={50}
+                            />
+                        </div>
+                    )}
                     <button
                         onClick={() => signOut()}
                         className="bg-purple-400 hover:bg-purple-500 text-xs font-bold py-2 px-4 rounded-full"
                     >
                         Sign Out
                     </button>
-                )}
-            </div>
+                </div>
+            )}
         </div>
     );
 };
