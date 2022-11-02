e1d52f4be8cace94e0d21efebc19573e8e596baf
Arslaan Qadus <azy2k9@gmail.com>
2022-11-02 22:34:08 +0000

    NEXT_AUTH_URL is optional, NEXT_AUTH_SECRET is required

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -8,8 +8,8 @@ import { z } from "zod";
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "production"]),
-  NEXTAUTH_SECRET: z.string().optional(),
-  NEXTAUTH_URL: z.string().url(),
+  NEXTAUTH_SECRET: z.string(),
+  NEXTAUTH_URL: z.string().url().optional(),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string()
 });
