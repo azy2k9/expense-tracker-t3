35382d7d1497ef896e4a1249a49839285cd19646
Arslaan Qadus <azy2k9@gmail.com>
2022-11-08 23:42:51 +0000

    Create first unit test with next-auth integration

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Homepage from '../src/pages/index';
+import { useSession } from 'next-auth/react';
+jest.mock('next-auth/react');
+
+const mockNullSession = null;
+
+describe('Home page', () => {
+    (useSession as jest.Mock).mockReturnValueOnce([mockNullSession, false]);
+
+    it('renders the homepage', () => {
+        render(<Homepage />);
+        const homepageText = screen.getByText(
+            /Track expenses and income to get an accurate and visual representation of your expenditure/i
+        );
+        expect(homepageText).toBeInTheDocument();
+    });
+});
