e7bdb2fe94c78deb31091acf9c9b3fb287735a53
azy2k9 <azy2k9@gmail.com>
2023-09-29 19:27:03 +0100

    Implement click outside handler for drawer staying open bug

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -2,6 +2,7 @@ import React, { forwardRef } from 'react';
 import { useTheme } from 'next-themes';
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
+import OutsideClickHandler from 'react-outside-click-handler';
 
 const DrawerButton = ({
   children,
@@ -42,31 +43,37 @@ const Drawer = forwardRef<HTMLDivElement, IProps>(function Drawer(
   const isSignedIn = session?.user?.id;
 
   return (
-    <div
-      ref={ref}
-      className={`dark:bg-slate-800 text-white bg-green-500 border-green-600 dark:border-slate-900 absolute right-0 top-0 w-full md:w-1/3 h-full z-10 transition-all duration-150 ease-out`}
+    <OutsideClickHandler
+      onOutsideClick={() => {
+        closeDrawer();
+      }}
     >
-      <div className="flex justify-center flex-col align-middle h-full md:h-[90vh] md:mt-[10vh]">
-        <DrawerButton
-          handleClick={() => router.push('/')}
-          closeDrawer={closeDrawer}
-        >
-          Home
-        </DrawerButton>
-        <DrawerButton
-          handleClick={() => router.push(isSignedIn ? '/signout' : '/signin')}
-          closeDrawer={closeDrawer}
-        >
-          Sign {isSignedIn ? 'Out' : 'In'}
-        </DrawerButton>
-        <DrawerButton
-          handleClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-          closeDrawer={closeDrawer}
-        >
-          {isDarkMode ? 'Light Mode' : 'Dark Mode'}
-        </DrawerButton>
+      <div
+        ref={ref}
+        className={`dark:bg-slate-800 text-white bg-green-500 border-green-600 dark:border-slate-900 absolute right-0 top-0 w-full md:w-1/3 h-full z-10 transition-all duration-150 ease-out`}
+      >
+        <div className="flex justify-center flex-col align-middle h-full md:h-[90vh] md:mt-[10vh]">
+          <DrawerButton
+            handleClick={() => router.push('/')}
+            closeDrawer={closeDrawer}
+          >
+            Home
+          </DrawerButton>
+          <DrawerButton
+            handleClick={() => router.push(isSignedIn ? '/signout' : '/signin')}
+            closeDrawer={closeDrawer}
+          >
+            Sign {isSignedIn ? 'Out' : 'In'}
+          </DrawerButton>
+          <DrawerButton
+            handleClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+            closeDrawer={closeDrawer}
+          >
+            {isDarkMode ? 'Light Mode' : 'Dark Mode'}
+          </DrawerButton>
+        </div>
       </div>
-    </div>
+    </OutsideClickHandler>
   );
 });
 
