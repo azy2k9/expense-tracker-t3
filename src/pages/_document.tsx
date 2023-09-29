952e0e4cd17b56220c61a979dd929fc6025809ca
azy2k9 <azy2k9@gmail.com>
2023-09-29 19:54:31 +0100

    quick win patch fix on scroll bar animation issue

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,22 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-    return (
-        <Html>
-            <Head>
-                <link rel="icon" href="/favicon.ico" />
-                <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-                <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-                {/* TODO: Enable this font in tailwind */}
-                {/* <link
+  return (
+    <Html className="overflow-x-hidden">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+        {/* TODO: Enable this font in tailwind */}
+        {/* <link
                     href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;500;700&display=swap"
                     rel="stylesheet"
                 /> */}
-            </Head>
-            <body className="dark:bg-slate-700 dark:text-white">
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+      </Head>
+      <body className="dark:bg-slate-700 dark:text-white">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
